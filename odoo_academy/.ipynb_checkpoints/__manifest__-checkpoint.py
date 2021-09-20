21c4995c59176a041bde42ba54be2c580f443f5c
sebastian2161 <sebastiancoll25@gmail.com>
2021-09-20 14:46:14 -0300

    work02

---
+++ odoo_academy/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Odoo Academy',
+    'summary': """Academy app to manage training""",
+    'description': """
+        Academy module to manage training:
+         -courses
+         -sessions
+         -attendes
+     """,
+    
+    'author': 'seba',
+    'website': 'https://www.odoo.com/',
+    'category': 'Training',
+    'version': '0.1',
+    'depends': ['base'],
+    'data': [
+        'security/academy_security.xml',
+        'security/ir.model.access.csv',
+        'views/academy_menuitems.xml',
+        'views/course_views.xml',
+        'views/session_views.xml',
+    ],
+    'demo': [
+        'demo/academy_demo.xml',
+    ],
+    
+}
+
+# Manifiesto de hoy 20-09-2021
